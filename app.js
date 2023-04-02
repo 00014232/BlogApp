c6881900f6da799505bf00981dd18114af9d9b3f
00014232 <116789388+00014232@users.noreply.github.com>
2023-04-02 17:49:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+
 app.set('views', './views');
 app.set('view engine', 'ejs');
 app.use('', require('./routes/articleRoutes'))
